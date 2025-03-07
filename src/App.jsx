b274bffbc749633eec67e97c6f52158fb4142e60
ifeanyisunday <sundayjnr10@gmail.com>
2025-03-07 10:46:20 +0100

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Designs from './designs/Designs';
 import Frontend from './frontend/frontend';
 import Backend from './backend/Backend';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
+import ContactInfo from './components/ContactInfo';
 
 
 const router = createHashRouter([
@@ -28,6 +30,10 @@ const router = createHashRouter([
       {
         path:'/projects',
         element: <Projects/>
+      },
+      {
+        path:'/contact',
+        element: <ContactInfo/>
       },
     ]
   );
